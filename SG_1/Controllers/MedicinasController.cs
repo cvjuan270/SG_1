cc5dfd09d17b6b5e557b16bdaf71c431d08e6cc3
unknown <cvjuan270@gmail.com>
2021-03-12 18:07:03 -0500

    avanzando

---
+++ SG_1/Controllers/MedicinasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SG_1.Models;
+
+namespace SG_1.Controllers
+{
+    public class MedicinasController : Controller
+    {
+        private SG_1Context db = new SG_1Context();
+
+        // GET: Medicinas
+        public ActionResult Index()
+        {
+            var medicina = db.Medicina.Include(m => m.Atenciones);
+            return View(medicina.ToList());
+        }
+
+        // GET: Medicinas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicina medicina = db.Medicina.Find(id);
+            if (medicina == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicina);
+        }
+
+        // GET: Medicinas/Create
+        public ActionResult Create()
+        {
+            ViewBag.AtenId = new SelectList(db.Atenciones, "Id", "Id");
+            return View();
+        }
+
+        // POST: Medicinas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,AtenId,HorIng,HorSal,Medico,Aptitu,FecApt,FecEnv,Coment,Observ,Alerta,UserName")] Medicina medicina)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Medicina.Add(medicina);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AtenId = new SelectList(db.Atenciones, "Id", "Local0", medicina.AtenId);
+            return View(medicina);
+        }
+
+        // GET: Medicinas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicina medicina = db.Medicina.Find(id);
+            if (medicina == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AtenId = new SelectList(db.Atenciones, "Id", "Local0", medicina.AtenId);
+            return View(medicina);
+        }
+
+        // POST: Medicinas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,AtenId,HorIng,HorSal,Medico,Aptitu,FecApt,FecEnv,Coment,Observ,Alerta,UserName")] Medicina medicina)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(medicina).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AtenId = new SelectList(db.Atenciones, "Id", "Local0", medicina.AtenId);
+            return View(medicina);
+        }
+
+        // GET: Medicinas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicina medicina = db.Medicina.Find(id);
+            if (medicina == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicina);
+        }
+
+        // POST: Medicinas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Medicina medicina = db.Medicina.Find(id);
+            db.Medicina.Remove(medicina);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ sql/DropAndCreate tablas.sql
@@ -0,0 +1,103 @@
+﻿USE [SG_1]
+GO
+
+
+DROP TABLE [dbo].[Admision];
+DROP TABLE Medicina
+DROP TABLE Auditoria
+DROP TABLE Interconsulta
+DROP TABLE Atenciones
+
+CREATE TABLE [dbo].[Admision] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [AtenId]   INT           NULL,
+    [HorIng]   TIME (7)      NULL,
+    [HorSal]   TIME (7)      NULL,
+    [Usuari]   VARCHAR (10)  NOT NULL,
+    [Pendie]   VARCHAR (200) NULL,
+    [UserName] VARCHAR (100) NULL
+);
+
+
+CREATE TABLE [dbo].[Auditoria] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [AtenId]   INT           NULL,
+    [ExaCom]   BIT           NOT NULL,
+    [ExaCom1]  VARCHAR (20)  NULL,
+    [DatInc]   BIT           NOT NULL,
+    [DatInc1]  VARCHAR (20)  NULL,
+    [AptErr]   BIT           NOT NULL,
+    [AptErr1]  VARCHAR (20)  NULL,
+    [FaFiMe]   BIT           NOT NULL,
+    [FaFiMe1]  VARCHAR (20)  NULL,
+    [FaFiPa]   BIT           NOT NULL,
+    [FaFiPa1]  VARCHAR (20)  NULL,
+    [Restri]   BIT           NOT NULL,
+    [Restri1]  VARCHAR (20)  NULL,
+    [Contro]   BIT           NOT NULL,
+    [Contro1]  VARCHAR (20)  NULL,
+    [Diagno]   BIT           NOT NULL,
+    [Diagno1]  VARCHAR (20)  NULL,
+    [ErrLle]   BIT           NOT NULL,
+    [ErrLle1]  VARCHAR (20)  NULL,
+    [ObNoRe]   VARCHAR (100) NULL,
+    [EmSnOb]   BIT           NOT NULL,
+    [EmSnOb1]  VARCHAR (20)  NULL,
+    [HorAud]   TIME (7)      NULL,
+    [FecAud]   DATE          NULL,
+    [Alerta]   INT           NULL,
+    [UserName] VARCHAR (100) NULL
+);
+
+
+CREATE TABLE [dbo].[Interconsulta] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [AtenId]   INT           NULL,
+    [NumLine]  INT           NULL,
+    [IntCon]   VARCHAR (50)  NULL,
+    [FecEnv]   DATE          NULL,
+    [PeEnCo]   VARCHAR (50)  NULL,
+    [EnHoIn]   BIT           NOT NULL,
+    [FeCoPa]   DATE          NULL,
+    [PeCoPa]   VARCHAR (50)  NULL,
+    [FeLeObs]  DATE          NULL,
+    [UserName] VARCHAR (100) NULL
+);
+
+
+CREATE TABLE [dbo].[Medicina] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [AtenId]   INT           NULL,
+    [HorIng]   TIME (7)      NULL,
+    [HorSal]   TIME (7)      NULL,
+    [Medico]   VARCHAR (50)  NULL,
+    [Aptitu]   VARCHAR (50)  NULL,
+    [FecApt]   DATE          NULL,
+    [FecEnv]   DATE          NULL,
+    [Coment]   VARCHAR (100) NULL,
+    [Observ]   VARCHAR (100) NULL,
+    [Alerta]   INT           NULL,
+    [UserName] VARCHAR (100) NULL
+);
+
+
+
+CREATE TABLE [dbo].[Atenciones] (
+    [Id]     INT           IDENTITY (1, 1) NOT NULL,
+    [Local0] VARCHAR (20)  NULL,
+    [TipExa] VARCHAR (20)  NULL,
+    [FecAte] DATE          NULL,
+    [NomApe] VARCHAR (100) NULL,
+    [DocIde] VARCHAR (20)  NULL,
+    [Empres] VARCHAR (100) NULL,
+    [SubCon] VARCHAR (100) NULL,
+    [Proyec] VARCHAR (100) NULL,
+    [Perfil] VARCHAR (100) NULL,
+    [Area]   VARCHAR (100) NULL,
+    [PueTra] VARCHAR (100) NULL,
+    [PeReAd] VARCHAR (50)  NULL,
+    [Hora]   TIME (7)      NULL,
+    [Medico] VARCHAR (250) NULL
+);
+
+
